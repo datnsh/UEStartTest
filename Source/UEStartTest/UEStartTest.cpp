a29c20209965721a03aaf069f479d5e5f278de86
LordVincent <datnsh@gmail.com>
2025-08-12 18:03:57 +0700

    Add include modules, setup input movement references

---
+++ Source/UEStartTest/UEStartTest.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UEStartTest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UEStartTest, "UEStartTest" );
